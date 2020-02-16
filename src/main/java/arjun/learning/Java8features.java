f3a2a4591a7db0357deb11ae74792ed552429550
Arjun Koroth <arjunkoroth@hotmail.com>
2020-02-16 15:51:50 +0530

    Java-8 Features Examples

--- src/main/java/arjun/learning/Java8features.java
+++ src/main/java/arjun/learning/Java8features.java
@@ -6,6 +6,10 @@ import arjun.learning.interfaces.ReadableInterface;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.StringJoiner;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -65,7 +69,104 @@ public class Java8features {
         rdConstructor.read("Arjun");
     }
 
-    public static void main(String[] args) {
-        methodReference();
+    public static void optioanlMethodExamples() {
+        String[] str = new String[10];
+        str[5] = "JAVA OPTIONAL CLASS EXAMPLE";  // Setting value for 5th index  
+        // It returns an empty instance of Optional class   
+        Optional<String> empty = Optional.empty();
+        System.out.println(empty);
+        // It returns a non-empty Optional  
+        Optional<String> value = Optional.of(str[5]);
+        // If value is present, it returns an Optional otherwise returns an empty Optional  
+        System.out.println("Filtered value: " + value.filter((s) -> s.equals("Abc")));
+        System.out.println("Filtered value: " + value.filter((s) -> s.equals("JAVA OPTIONAL CLASS EXAMPLE")));
+        // It returns value of an Optional. if value is not present, it throws an NoSuchElementException    
+        System.out.println("Getting value: " + value.get());
+        // It returns hashCode of the value  
+        System.out.println("Getting hashCode: " + value.hashCode());
+        // It returns true if value is present, otherwise false  
+        System.out.println("Is value present: " + value.isPresent());
+        // It returns non-empty Optional if value is present, otherwise returns an empty Optional  
+        System.out.println("Nullable Optional: " + Optional.ofNullable(str[5]));
+        // It returns value if available, otherwise returns specified value,  
+        System.out.println("orElse: " + value.orElse("Value is not present"));
+        System.out.println("orElse: " + empty.orElse("Value is not present"));
+        value.ifPresent(System.out::println);   // printing value by using method reference  
     }
+
+    private static void stringJoiners() {
+        // Joiner with a delimeter.
+        StringJoiner joiner1 = new StringJoiner(",");
+        joiner1.add("A");
+        joiner1.add("B");
+        joiner1.add("C");
+        System.out.println(joiner1);
+        // Joiner with delimiter, prefix and suffix.
+        StringJoiner joiner2 = new StringJoiner(":", "[", "]");
+        joiner2.add("D");
+        joiner2.add("E");
+        joiner2.add("F");
+        System.out.println(joiner2);
+        // Merger one joiner with another
+        StringJoiner merger = joiner1.merge(joiner2);
+        System.out.println(merger);
+        // Length
+        System.out.println("Length--" + merger.length());
+    }
+
+    private static void collectorAndStream() {
+        List<Product> productList = new ArrayList<Product>() {
+            {
+                add(new Product(10, "R15", 170000));
+                add(new Product(20, "MT15", 170000));
+                add(new Product(10, "Dominar", 250000));
+                add(new Product(10, "RS200", 170000));
+            }
+        };
+        List<Integer> productPriceList = productList.stream()
+                .map(p -> p.price)
+                .collect(Collectors.toList());
+        System.out.println(productPriceList);
+
+        List<Product> pList = productList.stream()
+                .filter(p -> p.price >= 250000)
+                .collect(Collectors.toList());
+        System.out.println(pList);
+
+        List<Integer> pL = productList.stream()
+                .filter(p -> p.price >= 250000)
+                .map(p -> p.price)
+                .collect(Collectors.toList());
+        System.out.println(pL);
+
+        Integer priceSum = productList.stream()
+                .collect(Collectors.summingInt(p -> p.price));
+        System.out.println("priceSum==" + priceSum);
+
+        productList.stream()
+                .filter(p -> p.price >= 250000)
+                .forEach(p -> {
+                    System.out.println(p.toString());
+                });
+        //Max and Min
+        Product maxP = productList.stream()
+                .max((p1, p2) -> p1.price > p2.price ? 1 : -1).get();
+        System.out.println("Maximum Price==" + maxP.toString());
+
+        Product minP = productList.stream()
+                .max((p1, p2) -> p1.price < p2.price ? 1 : -1).get();
+        System.out.println("Minimum Price==" + minP.toString());
+
+        Map<String, Integer> prodMap = productList.stream()
+                .collect(Collectors.toMap(p -> p.name, p -> p.price));
+        prodMap.entrySet().forEach(entry -> {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        });
+        
+    }
+
+    public static void main(String[] args) {
+        collectorAndStream();
+    }
+
 }
